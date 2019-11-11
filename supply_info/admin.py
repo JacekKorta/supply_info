1e773408a2eee678dbd40e5ae07f37addf810e49
JacekKorta <jacek.korta@gmail.com>
2019-11-11 19:20:51 +0100

    admin site configured

--- supply_info/admin.py
+++ supply_info/admin.py
@@ -1,6 +1,37 @@
 from django.contrib import admin
 from .models import ActiveProductList, PriceList, Product
 
-admin.site.register(PriceList)
-admin.site.register(Product)
-admin.site.register(ActiveProductList)
+class ActiveProductInLine(admin.StackedInline):
+    model = ActiveProductList
+    extra = 0
+
+
+class PriceListInLine(admin.TabularInline):
+    model = PriceList
+    extra = 0
+
+
+class ProductAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['code', 'name', 'additional_info']}),
+        ('Dodatkowe informacje:', {'fields': ['manufacturer', 'site_address'], 'classes': ['collapse']}),
+        ('Grupy:', {'fields': ['type', 'sub_type', 'mark'], 'classes': ['collapse']}),
+
+    ]
+    inlines = [ActiveProductInLine, PriceListInLine]
+
+    list_display = ('code', 'name', )
+
+
+class ActiveProductListAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['product_code', 'is_active']}),
+        ]
+    list_display = ('product_code', 'is_active')
+    list_filter = ['is_active']
+
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(ActiveProductList, ActiveProductListAdmin )
+
