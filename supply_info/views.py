f90b899c82e83e743699b873ecdba6f997bf41d0
JacekKorta <jacek.korta@gmail.com>
2019-12-02 21:58:08 +0100

    change Event.objects.filter...first() to .last()

--- supply_info/views.py
+++ supply_info/views.py
@@ -13,7 +13,7 @@ from .sp_modules import receiving_data, db_saves
 def index(request):
     # this page will be changed in the future
     machines = Product.objects.prefetch_related('price_lists', 'product_availability').filter(mark='M').order_by("code")
-    last_update_time = Event.objects.filter(event_name='availability update').first()
+    last_update_time = Event.objects.filter(event_name='availability update').last()
     return render(request, 'supply_info/machine_list.html', {'machines': machines,
                                                              'now': datetime.today().date(),
                                                              'last_update_time': last_update_time,
@@ -22,7 +22,7 @@ def index(request):
 
 def machine_list(request):
     machines = Product.objects.prefetch_related('price_lists', 'product_availability').filter(mark='M').order_by("code")
-    last_update_time = Event.objects.filter(event_name='availability update').first()
+    last_update_time = Event.objects.filter(event_name='availability update').last()
     return render(request, 'supply_info/machine_list.html', {'machines': machines,
                                                              'now': datetime.today().date(),
                                                              'last_update_time': last_update_time,
