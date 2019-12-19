0477d2da07cebb5e3347494be1bfcf4e5a62b7c4
JacekKorta <jacek.korta@gmail.com>
2019-12-19 20:47:00 +0100

    change serializer for product model

--- supply_info/serializers.py
+++ supply_info/serializers.py
@@ -7,7 +7,8 @@ class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     code = serializers.CharField(max_length=100)
     name = serializers.CharField(max_length=200)
-    prod_group = serializers.CharField(required=False, allow_blank=True, max_length=60)
+    type = serializers.CharField(required=False, allow_blank=True, max_length=30)
+    sub_type = serializers.CharField(required=False, allow_blank=True, max_length=30)
 
     def create(self, validated_data):
         return ProductSerializer.object.create(**validated_data)
@@ -15,4 +16,5 @@ class ProductSerializer(serializers.Serializer):
     def update(self, instance, validated_data):
         instance.code = validated_data.get('code', instance.code)
         instance.name = validated_data.get('name', instance.name)
-        instance.prod_group = validated_data.get('prod_group')
+        instance.type = validated_data.get('type', instance.type)
+        instance.sub_type = validated_data.get('sub_type', instance.sub_type)
