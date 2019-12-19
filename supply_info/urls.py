8aa4064b2af9e201cd65a81ccfcfdb2c8c758ab0
JacekKorta <jacek.korta@gmail.com>
2019-12-19 20:47:32 +0100

    add urls for product restapi

--- supply_info/urls.py
+++ supply_info/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('update_product_info/', views.update_product_info, name='update_product_info'),
     path('update_product_availability/', views.update_product_availability, name='update_product_availability'),
     path('search_product/', views.search_product, name='search_product'),
+    path('products/', views.product_list),
+    path('products/<code>', views.product_detail),
 ]
