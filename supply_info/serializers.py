726da644662ee6f1704cd68f3460c538ad84238a
JacekKorta <jacek.korta@gmail.com>
2019-12-22 21:39:16 +0100

    repaired instance save

--- supply_info/serializers.py
+++ supply_info/serializers.py
@@ -8,11 +8,11 @@ from django.contrib.auth.models import User
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     code = serializers.CharField(max_length=100)
-    name = serializers.CharField(max_length=200)
+    name = serializers.CharField(required=False, allow_blank=True, max_length=200)
     type = serializers.CharField(required=False, allow_blank=True, max_length=30)
     sub_type = serializers.CharField(required=False, allow_blank=True, max_length=30)
-    next_shipment = serializers.CharField(required=False, allow_blank=True, max_length=12)
-    site_address = serializers.CharField(required=False, allow_blank=True, max_length=120)
+    next_shipment = serializers.CharField(required=False, allow_blank=True, allow_null=True, max_length=12)
+    site_address = serializers.CharField(required=False, allow_blank=True, allow_null=True, max_length=120)
 
     def create(self, validated_data):
         return ProductSerializer.object.create(**validated_data)
@@ -24,6 +24,8 @@ class ProductSerializer(serializers.Serializer):
         instance.sub_type = validated_data.get('sub_type', instance.sub_type)
         instance.next_shipment = validated_data.get('next_shipment', instance.next_shipment)
         instance.site_address = validated_data.get('site_address', instance.site_address)
+        instance.save()
+        return instance
 
 
 class ProductAvailabilitySerializer(serializers.Serializer):
@@ -34,4 +36,6 @@ class ProductAvailabilitySerializer(serializers.Serializer):
     def update(self, instance, validated_data):
         instance.product_code = validated_data.get('product_code', instance.product_code)
         instance.availability = validated_data.get('availability', instance.availability)
+        instance.save()
+        return instance
 
