e79dbc417a0fa1f1adf2fb5af0bad0e2a2525433
JacekKorta <jacek.korta@gmail.com>
2020-01-26 20:56:51 +0100

    change: translate urls

--- supply_info/urls.py
+++ supply_info/urls.py
@@ -6,12 +6,12 @@ app_name = 'supply_info'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('machines_list/', views.machines_list, name='machines_list'),
-    path('products_list/<sub_type>', views.product_list, name='products_list'),
-    path('update_product_info/', views.update_product_info, name='update_product_info'),
-    path('update_product_availability/', views.update_product_availability, name='update_product_availability'),
-    path('search_product/', views.search_product, name='search_product'),
-    path('accounts/change_password/', views.change_password, name='change_password'),
+    path('lista-maszyn/', views.machines_list, name='machines_list'),
+    path('lista-produktow/<sub_type>', views.product_list, name='products_list'),
+    path('zasil-baze-produktow/', views.update_product_info, name='update_product_info'),
+    path('uaktualnij-stany/', views.update_product_availability, name='update_product_availability'),
+    path('wyszukaj-produkt/', views.search_product, name='search_product'),
+    path('konto/zmien-haslo/', views.change_password, name='change_password'),
     path('api/products/', views.ApiProductList.as_view()),
     path('api/products/<code>', views.ApiProductDetail.as_view()),
     path('api/availability/', views.ApiAvailabilityList.as_view()),
