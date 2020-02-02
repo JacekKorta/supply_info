492b9e486b45c066107a4f64b268ff938a06a572
JacekKorta <jacek.korta@gmail.com>
2020-02-02 15:17:39 +0100

    add: test for extract_data_to_register_machine func

--- serial_numbers/tests/test_modules.py
+++ serial_numbers/tests/test_modules.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 
 from serial_numbers.models import Machine
-from serial_numbers.sn_modules.sn_parser import extract_serial_numbers, parse_serials
+from serial_numbers.sn_modules.sn_parser import extract_data_to_register_machine, extract_serial_numbers, parse_serials
 
 
 class SetUpClass(TestCase):
@@ -12,17 +12,21 @@ class SetUpClass(TestCase):
 
 
 class ShipmentToCustomerTest(SetUpClass):
-    def test_extract_numbers(self):
+    def test_extract_numbers_works(self):
         data = 'A12345678\r\n123456789\r\n987654321\r\n12345678\r\n12345678910\r\n'
         expected_result = ['A12345678', '123456789', '987654321', '12345678', '12345678910', '']
         serials = extract_serial_numbers(data)
         self.assertEqual(serials, expected_result, 'The lists are not the same')
 
-    def test_parse_serial(self):
+    def test_parse_serial_works(self):
         data = ['A12345678', '123456789', '987654321', '12345678', '12345678910', '']
         expected_result = ['123456789', '987654321']
         serials = parse_serials(data)
         self.assertEqual(serials, expected_result, 'The lists are not the same')
 
-
+    def test_extract_data_to_register_works(self):
+        data = 'wrong,A12345678\ngood1,123456789 \ngood2, 987654321\n12345678\nwrong,12345678910\n'
+        expected_result = [('good1', '123456789'), ('good2', '987654321')]
+        machines = extract_data_to_register_machine(data)
+        self.assertEqual(machines, expected_result, 'The list are not the same')
 
