283cd899d726d34b5ebf32f7f462c859627768e1
JacekKorta <jacek.korta@gmail.com>
2020-02-08 20:01:40 +0100

    add: models for Issue and Comments

--- warranty_parts/models.py
+++ warranty_parts/models.py
@@ -5,39 +5,55 @@ from serial_numbers.models import Machine
 
 
 class Issues(models.Model):
-    WHERE_IS_THE_PART_CHOICES = []
-    FACTORY_STATUS_CHOICES = []
-    id = models.IntegerField(max_length=5, unique=True)
+    WHERE_IS_THE_PART_CHOICES = [
+        ('czeka_na_wymiane', 'czeka na wymianę'),
+        ('odrzucone', 'Odrzucone'),
+        ('wydana_z_maszyny', 'Wydana z maszyny'),
+        ('wymieniona','Wymieniona'),
+    ]
+    FACTORY_STATUS_CHOICES = [
+        ('czeka_na_wymiane', 'Czeka na wymianę'),
+        ('niezgloszone', 'Niezgłoszone'),
+        ('odrzucone', 'odrzucone'),
+        ('wymienione', 'Wymienione'),
+        ('zgloszone', 'Zgłoszone'),
+    ]
     time_stamp = models.DateTimeField(auto_now_add=True, verbose_name='Data utworzenia')
     customer = models.CharField(max_length=52, blank=True, null=True, verbose_name='Klient')
-    machine = models.ForeignKey('Machine', on_delete=models.CASCADE, verbose_name='Maszyna')
+    machine = models.ForeignKey(Machine,
+                                   to_field='serial_number',
+                                   on_delete=models.CASCADE,
+                                   null=True,
+                                   blank=True,
+                                   verbose_name='Maszyna',
+                                   related_name='warranty_parts_issue')
     part_number = models.CharField(max_length=24, verbose_name='Numer części')
     part_name = models.CharField(max_length=128, verbose_name='Nazwa części')
     quantity = models.CharField(max_length=3, verbose_name='Ilość')
     issue_description = models.TextField(blank=True,
                                          null=True,
                                          verbose_name='Opis usterki')
-    where_is_the_part = models.CharField(max_length=15,
+    where_is_the_part = models.CharField(max_length=20,
                                          choices=WHERE_IS_THE_PART_CHOICES,
                                          default='Czeka na wymianę',
                                          verbose_name='Status wymiany')
-    factory_status = models.CharField(max_length=15,
+    factory_status = models.CharField(max_length=20,
                                       choices=FACTORY_STATUS_CHOICES,
                                       default='Niezgłoszone',
                                       verbose_name='Status u producenta')
     doc_number = models.CharField(max_length=13, blank=True, null=True, verbose_name='Numer proformy')
 
     def __str__(self):
-        return self.id
+        return str(self.id)
 
 
 class Comments(models.Model):
     issue = models.ForeignKey('Issues', on_delete=models.CASCADE, related_name='comments', verbose_name='Komentarz')
-    username = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='Użytkownik')
+    username = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='Użytkownik', on_delete=models.CASCADE)
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    visible = models.BooleanField(default=True, verbose_name='Widoczny?')
+    inform_all = models.BooleanField(default=True, verbose_name='powiadom innych')
 
     class Meta:
         ordering = ('created',)
