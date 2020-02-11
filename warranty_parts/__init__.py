2ac9e2de4ae19ece7ddd25be6031f0f44f533fa2
Jacek Korta <jacek.korta@gmail.com>
2020-02-11 16:03:38 +0100

    Add: verbose name for warranty_parts app

---
+++ warranty_parts/__init__.py
@@ -0,0 +1,2 @@
+
+default_app_config = 'warranty_parts.apps.WarrantyPartsConfig'
--- warranty_parts/apps.py
+++ warranty_parts/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class WarrantyPartsConfig(AppConfig):
     name = 'warranty_parts'
+    verbose_name = '3. Części gwarancyjne'
