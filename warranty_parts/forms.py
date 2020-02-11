f3d19901c845f036f5507b85064fd43eb3c24feb
Jacek Korta <jacek.korta@gmail.com>
2020-02-11 16:04:56 +0100

    Add: comments to issues

--- warranty_parts/forms.py
+++ warranty_parts/forms.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django import forms
 from django.contrib.auth.models import User
 from serial_numbers.models import Machine
@@ -5,13 +6,14 @@ from warranty_parts.models import Comments
 
 
 class AddIssueForm(forms.Form):
+    today = datetime.now()
     customer = forms.CharField(label='Nazwa klienta')
     machine_serial_number = forms.CharField(label='Numer seryjny maszyny')
     part_number = forms.CharField(label='Numer cześci')
     part_name = forms.CharField(label='Nazwa części')
     quantity = forms.IntegerField(initial=1, label='Ilość')
     issue_description = forms.CharField(widget=forms.Textarea, label='Opis usterki')
-    document_number = forms.CharField(label='Numer proformy', required=False)
+    document_number = forms.CharField(label='Numer proformy', initial=f'{today.strftime("%y")}-FP/', required=False)
 
     """
     def clean_machine_serial_number(self):
@@ -20,9 +22,9 @@ class AddIssueForm(forms.Form):
             raise forms.ValidationError('Podana maszyna nie figuruje w bazie')
         return serial_number"""
 
-# dodaj użytkownika i numer zgłoszenia poza formularzem
+
 class AddCommentForm(forms.Form):
     body = forms.CharField(widget=forms.Textarea, label='komentarz')
-    inform_all = forms.BooleanField(required=False, initial=True)
+    inform_all = forms.BooleanField(required=False, initial=True, label='Powiadom innych')
 
 
--- warranty_parts/views.py
+++ warranty_parts/views.py
@@ -1,6 +1,8 @@
 from datetime import date
 from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
 from warranty_parts.forms import AddCommentForm, AddIssueForm
 from warranty_parts.wp_modules import db_save as wp_db_save
@@ -19,14 +21,18 @@ def add_issue(request):
 
 
 def add_comment(request, issue_id):
+    current_user = request.user
     if request.method == 'POST':
         default_data = {'user': request.user}
         form = AddCommentForm(request.POST, default_data)
         if form.is_valid():
             form_input = form.cleaned_data
-        return redirect('admin:warranty_parts_issues_<issue_id>_change')
+            wp_db_save.save_comment(form_input['body'],issue_id, current_user)
+        return HttpResponseRedirect(reverse('admin:warranty_parts_issues_change',
+                                            args=(issue_id,),
+                                            current_app='warranty_parts'))
     else:
         form = AddCommentForm()
-    return render(request, 'warranty_parts/add_issue.html', {'form': form,
+    return render(request, 'warranty_parts/add_comment.html', {'form': form,
                                                              'issue_id': issue_id,
-                                                             'title': 'Dodaj komentarz'})
+                                                             'user': current_user,})
