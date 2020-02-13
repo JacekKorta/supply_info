c93e55a14523db2ead88a3bb0e29679147d625ed
Jacek Korta <jacek.korta@gmail.com>
2020-02-13 15:48:40 +0100

    Edit: Now save_issues and save_comment returns the objects used for the mails sending functions.

--- warranty_parts/wp_modules/db_save.py
+++ warranty_parts/wp_modules/db_save.py
@@ -9,16 +9,18 @@ def save_issues(form_dict):
     except Machine.DoesNotExist:
         machine = None
         print(f'nie znaleziono "{machine_sn}"')
-    Issues.objects.create(customer=customer,
-                          machine=machine,
-                          part_number=part_number,
-                          part_name=part_name,
-                          quantity=quantity,
-                          issue_description=issue_description,
-                          doc_number=document_number)
+    issue = Issues.objects.create(customer=customer,
+                                  machine=machine,
+                                  part_number=part_number,
+                                  part_name=part_name,
+                                  quantity=quantity,
+                                  issue_description=issue_description,
+                                  doc_number=document_number)
+    return issue, machine
 
 
 def save_comment(body, issue_id, username):
-    Comments.objects.create(issue=Issues.objects.get(pk=issue_id),
-                            username=username,
-                            body=body)
+    comment = Comments.objects.create(issue=Issues.objects.get(pk=issue_id),
+                                      username=username,
+                                      body=body)
+    return comment
