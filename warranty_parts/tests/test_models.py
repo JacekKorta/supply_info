ecb1baeeb9ac2d5d6d9a950d12a8e25f1f0ad5bc
JacekKorta <jacek.korta@gmail.com>
2020-02-15 21:22:54 +0100

    Add: test for warranty parts models

--- warranty_parts/tests/test_models.py
+++ warranty_parts/tests/test_models.py
@@ -1,21 +1,23 @@
 from datetime import datetime
 
+from django.contrib.auth.models import User
 from django.test import TestCase
+
 from warranty_parts.models import Comments, Issues
 from serial_numbers.models import Machine
 
 
-
 class WarrantyPartsIssuesTest(TestCase):
     def setUp(self):
-        Machine.objects.crete(code='Machine_01',
-                              serial_number='123456789')
+        Machine.objects.create(code='Machine_01',
+                               serial_number='123456789')
 
     def create_issue(self, customer, serial_number, part_number, part_name, issue_description, doc_number):
-        current_datetime = datetime.now()
+        current_datetime = datetime.utcnow()
         return Issues.objects.create(customer=customer,
-                                     serial_number=serial_number,
+                                     machine=Machine.objects.get(serial_number=serial_number),
                                      part_number=part_number,
+                                     part_name=part_name,
                                      issue_description=issue_description,
                                      doc_number=doc_number,), current_datetime
 
@@ -29,13 +31,51 @@ class WarrantyPartsIssuesTest(TestCase):
         self.assertTrue(isinstance(sample_issue, Issues))
         self.assertTrue(isinstance(sample_issue.machine, Machine),
                         'The machine in issue is not an Machine class instance')
+        self.assertEqual(current_datetime.strftime('%Y-%m-%d - %H:%M'),
+                         sample_issue.time_stamp.strftime('%Y-%m-%d - %H:%M'))
         self.assertEqual('Customer_01', sample_issue.customer,'The customer codes are not the same')
-        self.assertEqual('Machine_01', sample_issue.machine, 'Should be Machine_01')
+        self.assertEqual('Machine_01', sample_issue.machine.code, 'Should be Machine_01')
+        self.assertEqual('123456789', sample_issue.machine.serial_number, 'The serials numbers are not correct')
         self.assertEqual('00000001', sample_issue.part_number, 'The parts numbers are not the same')
+        self.assertEqual('Part_01_name', sample_issue.part_name, 'The parts names are not the same')
         self.assertEqual('sample description', sample_issue.issue_description,
                          'The issues descriptions are not the same')
-        self.assertEqual('Czeka na wymianę', sample_issue.where_is_the_part, '"Where is the part" status is not correct')
-        self.assertEqual('Czeka na wymianę', sample_issue.factory_status, '"Factory status" is not correct')
+        self.assertEqual('czeka_na_wymiane', sample_issue.where_is_the_part, '"Where is the part" status is not correct')
+        self.assertEqual('niezgloszone', sample_issue.factory_status, '"Factory status" is not correct')
         self.assertEqual(1, sample_issue.quantity, 'Quantity is ont the same')
         self.assertEqual('20-FP/0001', sample_issue.doc_number, 'The documents number are not the same')
-        self.assertEqual(None, sample_issue.reques, 'The request status are not the same')
+        self.assertEqual(None, sample_issue.request, 'The request status are not the same')
+        self.assertEqual(str(sample_issue), str(sample_issue.id))
+
+
+class WarrantyPartsCommentsTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user('adam', 'adam@example.com', 'adampassword')
+        self.client.login(username='adam', password='adampassword')
+        Machine.objects.create(code='Machine_01',
+                               serial_number='123456789')
+        Issues.objects.create(customer='Customer_01',
+                                       machine=Machine.objects.get(serial_number='123456789'),
+                                       part_number='00000001',
+                                       issue_description='sample description',
+                                       part_name='Part_01_name',
+                                       doc_number='20-FP/0001')
+
+    def create_comment(self, user, issues_id, body):
+        current_datetime = datetime.utcnow()
+        return Comments.objects.create(issue=Issues.objects.get(id=issues_id),
+                                       username=User.objects.get(username=user.username),
+                                       body=body), current_datetime
+
+    def test_correct_comment_creation(self):
+        sample_comment, current_datetime = self.create_comment(user=self.user,
+                                                               issues_id=1,
+                                                               body='Sample comment body')
+        self.assertTrue(isinstance(sample_comment, Comments))
+        self.assertTrue(isinstance(sample_comment.username, User))
+        self.assertTrue(isinstance(sample_comment.issue, Issues))
+        self.assertEqual('Sample comment body', sample_comment.body)
+        self.assertEqual(current_datetime.strftime('%Y-%m-%d - %H:%M'),
+                         sample_comment.created.strftime('%Y-%m-%d - %H:%M'))
+        self.assertEqual(str(sample_comment), 'Comment by adam on 1')
+
