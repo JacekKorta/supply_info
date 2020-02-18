2cc7ecb9c5d69ed914f0bf6fccc181b5e53a384f
Jacek Korta <jacek.korta@gmail.com>
2020-02-18 13:22:12 +0100

    Add: actions for issues list in admin panel (csv report and change status)

--- warranty_parts/admin.py
+++ warranty_parts/admin.py
@@ -1,4 +1,8 @@
+import csv
+from datetime import datetime
+
 from django.contrib import admin
+from django.http import HttpResponse
 from django.utils.html import format_html
 from django.urls import reverse
 
@@ -57,6 +61,41 @@ class IssuesAdmin(admin.ModelAdmin):
         return format_html(
             '<a href="{}">Dodaj komentarz</a>', url, obj)
 
+    def change_factory_status_to_reported(self, request, queryset):
+        queryset.update(factory_status='zgloszone')
+
+    def change_request_status(self, request, queryset):
+        queryset.update(request=True)
+
+    def create_csv_report(self, request, queryset):
+        today = datetime.now()
+        file_name = f'Warranty_parts_{today.strftime("%Y_%m_%d")}.csv'
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = f'attachment; filename={file_name}'
+        response.write(u'\ufeff'.encode('utf8'))
+        writer = csv.writer(response)
+        writer.writerow(['ID',
+                         'Machine model',
+                         'Serial number',
+                         'Part number',
+                         'Qty',
+                         'Part name',
+                         'Issue description',
+                         'Reply',
+                         'Request'])
+        for issue in queryset:
+            writer.writerow([issue.id,
+                             issue.machine.code,
+                             issue.machine.serial_number,
+                             issue.part_number,
+                             issue.quantity,
+                             issue.part_name,
+                             issue.issue_description])
+        return response
+
+    create_csv_report.short_description = 'Pobierz raport'
+    change_factory_status_to_reported.short_description = 'Oznacz jako: Zgłoszone'
+    change_request_status.short_description = 'Oznacz jako: Do zwrotu'
     get_machine_serial_number.short_description = 'Numer seryjny'
     get_machine_serial_number.ordering = 'serial_number'
     get_machine_code.short_description = 'Model maszyny'
@@ -64,6 +103,7 @@ class IssuesAdmin(admin.ModelAdmin):
     get_comments_sum.short_description = 'Komentarze'
     get_add_comment_link.short_description = 'Dodaj komentarz'
 
+    actions = [change_factory_status_to_reported, change_request_status, create_csv_report]
     readonly_fields = ('id', 'time_stamp')
     fieldsets = [
         (None, {'fields': ['id',
