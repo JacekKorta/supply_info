2a1e14ba0bdfabab3abebb961fa27b534194fad0
JacekKorta <jacek.korta@gmail.com>
2020-03-08 12:15:39 +0100

    Update: forms after styling added

--- serial_numbers/forms.py
+++ serial_numbers/forms.py
@@ -5,7 +5,7 @@ from serial_numbers.models import Customer
 
 class ShipmentForm(forms.Form):
     customer = forms.ModelChoiceField(queryset=Customer.objects.all().order_by('name'), label='Klient')
-    delivery_note_number = forms.CharField(label='Nr dokumentu WZ')
+    delivery_note_number = forms.CharField(label='Nr dokumentu WZ (bez serii i roku):')
     shipment = forms.CharField(label='Numery seryjne', widget=forms.Textarea)
 
 
