7518bf5ebcf2d2d1f82f4e57a454c394d2151806
JacekKorta <jacek.korta@gmail.com>
2020-03-14 18:44:57 +0100

    add new links to the left navbar in warranty parts section

--- warranty_parts/admin.py
+++ warranty_parts/admin.py
@@ -6,7 +6,6 @@ from django.contrib import admin
 from django.http import FileResponse, HttpResponse
 from django.utils.html import format_html
 from django.urls import reverse
-from django.shortcuts import render
 from reportlab.pdfgen import canvas
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
@@ -105,7 +104,7 @@ class IssuesAdmin(admin.ModelAdmin):
                                  '',
                                  issue.quantity,
                                  issue.part_name,
-                                 issue.issue_description])
+                                 issue.eng_issue_description])
         return response
 
     def issue_to_pdf(self, request, queryset):
