0b92a99e2ded764aa0bd2c5e93aa36ed3c94232d
JacekKorta <jacek.korta@gmail.com>
2020-03-15 23:13:10 +0100

    register app and urls

--- public_python/local_settings.py
+++ public_python/local_settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.postgres',
     'coverage',
+    'info_channel',
     'supply_info',
     'serial_numbers',
     'rest_framework',
--- public_python/urls.py
+++ public_python/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('', include('serial_numbers.urls')),
     path('', include('supply_info.urls')),
     path('', include('warranty_parts.urls')),
+    path('info_channel/', include('info_channel.urls')),
     path('admin/', admin.site.urls),
     path('accounts/login/', views.LoginView.as_view(), name='login'),
     path('accounts/logout', views.LogoutView.as_view(next_page='/'), name='logout'),
