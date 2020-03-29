0b0abc1004d1e6958ec828545ad686a36cd8ece8
JacekKorta <jacek.korta@gmail.com>
2020-03-29 12:24:35 +0200

    add view for posts list

--- info_channel/views.py
+++ info_channel/views.py
@@ -1,4 +1,3 @@
-
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.views.generic import DetailView, ListView
@@ -10,10 +9,24 @@ from info_channel.models import Post
 class PostListView(ListView):
 
     def get_queryset(self):
-        if self.request.user.is_staff:
-            return Post.published.prefetch_related('post_body_paragraph').all()
+        category = self.kwargs.get('category')
+        if category == 'all':
+            if self.request.user.is_staff:
+                return Post.published.prefetch_related('post_body_paragraph').all()
+            else:
+                return Post.published.prefetch_related('post_body_paragraph').exclude(category='inner_information')
         else:
-            return Post.published.prefetch_related('post_body_paragraph').exclude(category='inner_information')
+            if self.request.user.is_staff:
+                return Post.published.prefetch_related('post_body_paragraph').filter(category=category)
+            else:
+                return Post.published.prefetch_related('post_body_paragraph')\
+                    .exclude(category='inner_information')\
+                    .filter(category=category)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category'] = self.kwargs.get('category')
+        return context
 
     paginate_by = 5
     context_object_name = 'posts'
