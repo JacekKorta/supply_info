a2f76bdd740902729ca82ef28f97ac90be6c2d5a
JacekKorta <jacek.korta@gmail.com>
2020-04-05 18:07:41 +0200

    add verbose names for IC models att

--- info_channel/models.py
+++ info_channel/models.py
@@ -20,21 +20,24 @@ class Post (models.Model):
         ('inner_information', 'Komunikaty firmowe'),
         ('news', 'Nowości'),
     )
-    title = models.CharField(max_length=250)
+    title = models.CharField(max_length=250, verbose_name='Tytuł')
     slug = models.SlugField(max_length=250, unique_for_date='publish')
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    publish = models.DateTimeField(default=timezone.now)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Autor')
+    publish = models.DateTimeField(default=timezone.now, verbose_name='Publikacja')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Utworzono')
+    updated = models.DateTimeField(auto_now=True, verbose_name='Aktualizacja')
     category = models.CharField(max_length=22,
                                 choices=CATEGORY_CHOICES,
-                                default='inner_information')
+                                default='inner_information',
+                                verbose_name='Kategoria')
     status = models.CharField(max_length=12,
                               choices=STATUS_CHOICES,
                               default='draft')
 
     class Meta:
         ordering = ('-publish',)
+        verbose_name = 'Wiadomość'
+        verbose_name_plural = 'Wiadomości'
 
     def __str__(self):
         return self.title
@@ -57,7 +60,14 @@ class PostBodyParagraph(models.Model):
     )
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post_body_paragraph')
     body = models.TextField()
-    img_position = models.CharField(max_length=7, choices=IMAGE_POSITION_ChOICES, default='center')
+    img_position = models.CharField(max_length=7,
+                                    choices=IMAGE_POSITION_ChOICES,
+                                    default='center',
+                                    verbose_name='Pozycja grafiki')
     img_address = models.CharField(max_length=250, blank=True, null=True, verbose_name='Adres grafiki')
     img_link = models.URLField(max_length=250, blank=True, null=True, verbose_name='Link grafiki')
     img_alt = models.CharField(max_length=40, blank=True, null=True, verbose_name='Tekst alternatywny')
+
+    class Meta:
+        verbose_name = 'Blok'
+        verbose_name_plural = 'Bloki'
