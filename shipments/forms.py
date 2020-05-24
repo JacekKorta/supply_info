d4b103688495cb746c7c70c9c4c68fc0c8426dd9
JacekKorta <jacek.korta@gmail.com>
2020-05-24 21:07:00 +0200

    adds add shipment form

---
+++ shipments/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from django.forms import BaseFormSet
+from django.forms import IntegerField
+
+from supply_info.models import Product
+from shipments.models import ShipmentDetail, Shipment
+
+
+class NewShipmentDetailForm(forms.Form):
+    product = forms.CharField(widget=forms.TextInput)
+    quantity = forms.IntegerField(widget=forms.NumberInput)
+
+
+
+
+
+
+
+
+
+
+
