d4f8fbeecfd7a37c69982d12eceed8d0ead260a6
JacekKorta <jacek.korta@gmail.com>
2020-07-11 15:03:37 +0200

    adds tests for edit alert

--- supply_info/tests/setUp.py
+++ supply_info/tests/setUp.py
@@ -8,15 +8,13 @@ from supply_info.models import Alert, Product, ProductAvailability, ActiveProduc
 
 class BasicSetup(TestCase):
     def setUp(self):
-        p = Product.objects.create(
+        Product.objects.create(
             code='Machine1',
             name='machine_1 desc',
             type='maszyny',
             mark='M'
         )
-        p.save()
-        a = ProductAvailability(product_code=Product.objects.get(code="Machine1"), availability=10)
-        a.save()
+        ProductAvailability.objects.create(product_code=Product.objects.get(code="Machine1"), availability=10)
         a = ActiveProductList(product_code=Product.objects.get(code="Machine1"))
         a.is_active = True
         a.save()
@@ -31,16 +29,15 @@ class BasicSetup(TestCase):
         b = ActiveProductList(product_code=Product.objects.get(code="Machine2"))
         b.is_active = True
         b.save()
-        Alert(user=self.user,
-              product=Product.objects.get(code='Machine1'),
-              qty_alert_lvl=10
-              )
-        Alert(user=self.user,
-              product=Product.objects.get(code='Machine2'),
-              less_or_equal=False,
-              qty_alert_lvl=4
-              )
-
+        Alert.objects.create(user=self.user,
+                             product=Product.objects.get(code='Machine1'),
+                             qty_alert_lvl=10
+                             )
+        Alert.objects.create(user=self.user,
+                             product=Product.objects.get(code='Machine2'),
+                             less_or_equal=False,
+                             qty_alert_lvl=4
+                             )
 
 class LoggedInTestCase(BasicSetup):
     def setUp(self):
--- supply_info/tests/test_alerts.py
+++ supply_info/tests/test_alerts.py
@@ -12,6 +12,10 @@ class BasicViewTest(LoggedInTestCase):
         response = self.client.get(reverse('supply_info:alerts_list_view'))
         self.assertEqual(response.status_code, 200)
 
+    def test_correct_template_is_used_on_alerts_list_view(self):
+        response = self.client.get(reverse('supply_info:alerts_list_view'))
+        self.assertTemplateUsed(response, 'supply_info/alerts_list.html')
+
     def test_alerts_are_visible_on_the_list(self):
         response = self.client.get(reverse('supply_info:alerts_list_view'))
         self.assertContains(response, 'Machine1')
@@ -20,3 +24,14 @@ class BasicViewTest(LoggedInTestCase):
         self.assertContains(response, 'Machine2')
         self.assertContains(response, '4')
         self.assertContains(response, 'więcej niż')
+
+    def test_alert_edit_page_works(self):
+        response = self.client.get(reverse('supply_info:alert_edit_view', kwargs={'alert_pk': 1}))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Machine1')
+        self.assertContains(response, '10')
+        self.assertContains(response, 'Mniej lub równe')
+
+
+
+
