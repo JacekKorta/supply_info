a9e8a20d1ec0cbac41ce2f7b4c36110a5e044abb
Jacek Korta <jacek.korta@gmail.com>
2020-09-11 09:32:49 +0200

    dummy: preapred for tests

--- supply_info/tests/test_views.py
+++ supply_info/tests/test_views.py
@@ -1,10 +1,36 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from django.test import TestCase
 from django.urls import reverse
+
+from supply_info.models import Product, PriceList, ProductAvailability, ActiveProductList
 
 
 class LoggedInTestCase(TestCase):
     def setUp(self):
+        Product.objects.create(
+            code='Machine1',
+            name='machine_1 desc',
+            type='maszyny',
+            mark='M'
+        )
+        ProductAvailability.objects.create(product_code=Product.objects.get(code="Machine1"), availability=10)
+        a = ActiveProductList(product_code=Product.objects.get(code="Machine1"))
+        a.is_active = True
+        a.save()
+        p = PriceList(product_code=Product.objects.get(code='Machine1'),
+                      price_a=10,
+                      price_b=5,
+                      price_c=7,
+                      price_d=15,)
+        p.save()
+
+        group_name = "Dystrybutorzy_B"
+        self.group_b = Group(name=group_name)
+        self.group_b.save()
+        group_name = "Dystrybutorzy_C"
+        self.group_c = Group(name=group_name)
+        self.group_c.save()
+
         self.user = User.objects.create_user('adam', 'adam@example.com', 'adampassword')
         self.client.login(username='adam', password='adampassword')
 
@@ -59,6 +85,15 @@ class TestPageLogged(LoggedInTestCase):
     def test_update_product_availability_works(self):
         response = self.client.get(reverse('supply_info:update_product_availability'))
         self.assertEqual(response.status_code, 302)
+
+    """
+    def test_search_view_user_group_b(self):
+        self.user.groups.add(self.group_b)
+        self.user.save()
+        results = Product.objects.prefetch_related('price_lists',
+                                                   'product_availability').filter(code='Machine1').order_by('code')
+        response = self.client.get(reverse('supply_info:search_product'))
+    """
 
 
 class TestPageAnonymous(TestCase):
