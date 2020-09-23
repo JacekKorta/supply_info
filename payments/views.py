217d1f10c0adb07be082ddb7de722ecf5c07c8c3
Jacek Korta <jacek.korta@gmail.com>
2020-09-23 11:23:21 +0200

    adds sending payments notification view

--- payments/views.py
+++ payments/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render, redirect
 from payments.forms import InvoiceDataForm
 from payments.contrib import payments_mod
 from payments.contrib.payments_session import Session
+from payments.contrib.payments_email import Email
 
 
 @staff_member_required
@@ -27,9 +28,18 @@ def read_invoices_data(request):
 @staff_member_required
 def delayed_invoices_handle(request):
     delayed_invoices = request.session['delayed_invoices']
-    print(delayed_invoices)
     if request.method == 'POST':
         if 'delete' in request.POST:
             Session.remove_customer(request, request.POST['delete'])
-
+        if 'send' in request.POST:
+            email = Email.get_customer_email(request.POST['send'])
+            if '@' not in email:
+                messages.success(request, f' email: {email}')
+            else:
+                data = request.session['delayed_invoices'][request.POST['send']]
+                customer_name = request.POST['send']
+                result = Email.send_payment_notification(email, data, customer_name)
+                if "Wysłano wiadomosć " in result:
+                    Session.remove_customer(request, request.POST['send'])
+                messages.success(request, result)
     return render(request, 'payments/delayed_payments_list.html', {'delayed_invoices': delayed_invoices})
