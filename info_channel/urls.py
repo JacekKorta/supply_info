76d0160facc40ea781bd7719b058f7af2b76c051
Jacek Korta <jacek.korta@gmail.com>
2020-09-25 08:42:13 +0200

    fix info channel urls

--- info_channel/urls.py
+++ info_channel/urls.py
@@ -6,7 +6,7 @@ app_name = 'info_channel'
 
 urlpatterns = [
     # post views
-    path('', views.PostListView.as_view(), name='post_list'),
+    path('informacje/', views.PostListView.as_view(), name='post_list'),
     path('informacje/<category>/', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:slug>/', views.PostDetailView.as_view(), name='post_detail'),
     ]
